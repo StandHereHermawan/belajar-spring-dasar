baba5b4bb9bd003a09e976d2756e54dc981142e5
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-08 19:36:34 +0700

    ApplicationContext, ApplicationContextTest

---
+++ src/test/java/ariefbelajarteknologi/spring/core/ApplicationContextTest.java
@@ -0,0 +1,17 @@
+package ariefbelajarteknologi.spring.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextTest {
+
+    @Test
+    void testApplicationContext() {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+
+        Assertions.assertNotNull(context);
+
+    }
+}
