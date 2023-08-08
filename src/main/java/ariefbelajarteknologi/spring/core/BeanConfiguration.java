9888878ec68c82bba50c9fea984fffc55e257738
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-08 20:03:00 +0700

    Bean, add BeanConfiguration fixed

--- src/main/java/ariefbelajarteknologi/spring/core/BeanConfiguration.java
+++ src/main/java/ariefbelajarteknologi/spring/core/BeanConfiguration.java
@@ -1,11 +1,13 @@
 package ariefbelajarteknologi.spring.core;
 
 import ariefbelajarteknologi.spring.core.data.Foo;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanConfiguration {
 
+    @Bean
     public Foo foo(){
         Foo foo = new Foo();
         return foo;
