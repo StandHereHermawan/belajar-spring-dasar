75f2278ea61df36f2a38559d4e3c28b6da0f7402
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-08 19:48:05 +0700

    Application Context, ApplicationContext fixed Test

--- src/test/java/ariefbelajarteknologi/spring/core/ApplicationContextTest.java
+++ src/test/java/ariefbelajarteknologi/spring/core/ApplicationContextTest.java
@@ -2,6 +2,7 @@ package ariefbelajarteknologi.spring.core;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ApplicationContextTest {
@@ -9,7 +10,7 @@ public class ApplicationContextTest {
     @Test
     void testApplicationContext() {
 
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 
         Assertions.assertNotNull(context);
 
