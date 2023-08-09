d1cf01e567c383c7b150c0c3d6efb03941abe313
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-09 15:57:01 +0700

    Dependency Injection, DependencyInjection with DependencyInjection Test

--- src/test/java/ariefbelajarteknologi/spring/core/DependencyInjectionTest.java
+++ src/test/java/ariefbelajarteknologi/spring/core/DependencyInjectionTest.java
@@ -4,9 +4,31 @@ import ariefbelajarteknologi.spring.core.data.Bar;
 import ariefbelajarteknologi.spring.core.data.Foo;
 import ariefbelajarteknologi.spring.core.data.FooBar;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class DependencyInjectionTest {
+
+    private ApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(DependencyInjectionConfiguration.class);
+    }
+
+    @Test
+    void testDepencencyInjection() {
+
+        Foo foo = applicationContext.getBean(Foo.class);
+        Bar bar = applicationContext.getBean(Bar.class);
+        FooBar fooBar = applicationContext.getBean(FooBar.class);
+
+        Assertions.assertSame(foo,fooBar.getFoo());
+        Assertions.assertSame(bar,fooBar.getBar());
+
+    }
 
     @Test
     void testWithoutDependencyInjection() {
