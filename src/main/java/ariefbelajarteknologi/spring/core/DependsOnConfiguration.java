f9ceac57581a86b379499a8534d8580d52535139
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-09 20:25:26 +0700

    Depends On, add class

---
+++ src/main/java/ariefbelajarteknologi/spring/core/DependsOnConfiguration.java
@@ -0,0 +1,25 @@
+package ariefbelajarteknologi.spring.core;
+
+import ariefbelajarteknologi.spring.core.data.Bar;
+import ariefbelajarteknologi.spring.core.data.Foo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class DependsOnConfiguration {
+
+    @Bean
+    public Foo foo(){
+        log.info("Create new foo");
+        return new Foo();
+    }
+
+    @Bean
+    public Bar bar(){
+        log.info("Create new bar");
+        return new Bar();
+    }
+
+}
