a6ef2edaf10e98d113c2b3a1f0dcd0228eee1218
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-10 15:22:39 +0700

    Membuat Scope, modify ScopeConfiguration class

--- src/main/java/ariefbelajarteknologi/spring/core/ScopeConfiguration.java
+++ src/main/java/ariefbelajarteknologi/spring/core/ScopeConfiguration.java
@@ -1,7 +1,10 @@
 package ariefbelajarteknologi.spring.core;
 
+import ariefbelajarteknologi.spring.core.data.Bar;
 import ariefbelajarteknologi.spring.core.data.Foo;
+import ariefbelajarteknologi.spring.core.scope.DoubletonScope;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -15,5 +18,19 @@ public class ScopeConfiguration {
     public Foo foo(){
         log.info("Create new foo");
         return new Foo();
+    }
+
+    @Bean
+    public CustomScopeConfigurer customScopeConfigurer(){
+        CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+        configurer.addScope("doubleton",new DoubletonScope());
+        return configurer;
+    }
+
+    @Bean
+    @Scope("doubleton")
+    public Bar bar(){
+        log.info("Create new Bar");
+        return new Bar();
     }
 }
