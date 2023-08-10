48631b4ed7c7e381cb5ff795d018cf61590e5b8c
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-10 16:31:51 +0700

    Life Cycle, Close connection LifeCycleTest

--- src/test/java/ariefbelajarteknologi/spring/core/LifeCycleTest.java
+++ src/test/java/ariefbelajarteknologi/spring/core/LifeCycleTest.java
@@ -1,18 +1,25 @@
 package ariefbelajarteknologi.spring.core;
 
 import ariefbelajarteknologi.spring.core.data.Connection;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class LifeCycleTest {
 
-    private ApplicationContext applicationContext;
+    private ConfigurableApplicationContext applicationContext;
 
     @BeforeEach
     void setUp() {
         applicationContext = new AnnotationConfigApplicationContext(LifeCycleConfiguration.class);
+        applicationContext.registerShutdownHook();
+    }
+
+    @AfterEach
+    void tearDown() {
+        // applicationContext.close();
     }
 
     @Test
