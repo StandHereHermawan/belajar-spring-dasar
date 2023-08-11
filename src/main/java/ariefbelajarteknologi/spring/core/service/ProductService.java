dcd38b0716f244e770f825ef4fb6fec2f95a195c
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-11 20:53:42 +0700

    Component, add comments

--- src/main/java/ariefbelajarteknologi/spring/core/service/ProductService.java
+++ src/main/java/ariefbelajarteknologi/spring/core/service/ProductService.java
@@ -1,7 +1,11 @@
 package ariefbelajarteknologi.spring.core.service;
 
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+// @Scope("prototype")
+// @Lazy
 @Component
 public class ProductService {
 }
