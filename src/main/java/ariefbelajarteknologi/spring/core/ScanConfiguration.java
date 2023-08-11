882221ec3e48a02c8a140eea47ae836d97e7ddad
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-11 20:29:33 +0700

    Component Scan, add class

---
+++ src/main/java/ariefbelajarteknologi/spring/core/ScanConfiguration.java
@@ -0,0 +1,11 @@
+package ariefbelajarteknologi.spring.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({
+        "ariefbelajarteknologi.spring.core.configuration"
+})
+public class ScanConfiguration {
+}
