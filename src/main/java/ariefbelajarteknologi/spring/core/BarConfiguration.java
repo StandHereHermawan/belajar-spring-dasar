43427af9f3269bb123179b98aa7a24bb81ae43f2
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-11 20:09:36 +0700

    Import, add class

---
+++ src/main/java/ariefbelajarteknologi/spring/core/BarConfiguration.java
@@ -0,0 +1,14 @@
+package ariefbelajarteknologi.spring.core;
+
+import ariefbelajarteknologi.spring.core.data.Bar;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BarConfiguration {
+
+    @Bean
+    public Bar bar(){
+        return new Bar();
+    }
+}
