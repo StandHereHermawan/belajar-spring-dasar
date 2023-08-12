dfc077da59d0ecac92fbfddd0a752677a3cfe902
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-12 17:45:32 +0700

    Constructor-based Dependency Injection, modify ProductService

--- src/main/java/ariefbelajarteknologi/spring/core/service/ProductService.java
+++ src/main/java/ariefbelajarteknologi/spring/core/service/ProductService.java
@@ -1,11 +1,19 @@
 package ariefbelajarteknologi.spring.core.service;
 
-import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Scope;
+import ariefbelajarteknologi.spring.core.repository.ProductRepository;
+import lombok.Getter;
 import org.springframework.stereotype.Component;
 
 // @Scope("prototype")
 // @Lazy
 @Component
 public class ProductService {
+
+    @Getter
+    private ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
 }
