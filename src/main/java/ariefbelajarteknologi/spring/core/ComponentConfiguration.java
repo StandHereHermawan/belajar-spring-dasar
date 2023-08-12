671809a32a83ec6e43d5a14d857c91bec3030d26
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-12 17:46:27 +0700

    Constructor-based Dependency Injection, modify ComponentConfiguration

--- src/main/java/ariefbelajarteknologi/spring/core/ComponentConfiguration.java
+++ src/main/java/ariefbelajarteknologi/spring/core/ComponentConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan({
+        "ariefbelajarteknologi.spring.core.repository",
         "ariefbelajarteknologi.spring.core.service"
 })
 public class ComponentConfiguration {
