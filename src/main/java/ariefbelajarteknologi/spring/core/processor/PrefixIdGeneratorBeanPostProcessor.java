c77d91744441159860665d3c7b08835a5e339bfd
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-17 02:37:34 +0700

    Ordered, add processor class

---
+++ src/main/java/ariefbelajarteknologi/spring/core/processor/PrefixIdGeneratorBeanPostProcessor.java
@@ -0,0 +1,30 @@
+package ariefbelajarteknologi.spring.core.processor;
+
+import ariefbelajarteknologi.spring.core.aware.IdAware;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class PrefixIdGeneratorBeanPostProcessor implements BeanPostProcessor, Ordered {
+
+    @Override
+    public int getOrder() {
+        return 2;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        log.info("Prefix Id Generator Processor for Bean {}", beanName);
+        if (bean instanceof IdAware) {
+            log.info("Prefix Set Id Generator for Bean {}", beanName);
+            IdAware idAware = (IdAware) bean;
+            idAware.setId("PREFIX - " + idAware.getId());
+        }
+
+        return bean;
+    }
+}
