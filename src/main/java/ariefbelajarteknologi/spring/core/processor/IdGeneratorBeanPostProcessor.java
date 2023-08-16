d7e79579170ab9b1fecf59fc5ebf57719cd567e2
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-17 02:33:43 +0700

    Ordered, modify class IdGeneratorBeanPostProcessor

--- src/main/java/ariefbelajarteknologi/spring/core/processor/IdGeneratorBeanPostProcessor.java
+++ src/main/java/ariefbelajarteknologi/spring/core/processor/IdGeneratorBeanPostProcessor.java
@@ -4,13 +4,19 @@ import ariefbelajarteknologi.spring.core.aware.IdAware;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
 @Slf4j
 @Component
-public class IdGeneratorBeanPostProcessor implements BeanPostProcessor {
+public class IdGeneratorBeanPostProcessor implements BeanPostProcessor, Ordered {
+
+    @Override
+    public int getOrder() {
+        return 1;
+    }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
