eed612196cc0c0dae686fa6cef09fcad934ae881
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-18 03:01:06 +0700

    Event Listener, modify data class

--- src/main/java/ariefbelajarteknologi/spring/core/data/User.java
+++ src/main/java/ariefbelajarteknologi/spring/core/data/User.java
@@ -1,8 +1,10 @@
 package ariefbelajarteknologi.spring.core.data;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class User {
 
     private String username;
