28378255ec33b856b674e6e46cea0aa2174617d9
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-18 03:07:01 +0700

    Event Listener, modify service class

--- src/main/java/ariefbelajarteknologi/spring/core/service/UserService.java
+++ src/main/java/ariefbelajarteknologi/spring/core/service/UserService.java
@@ -1,5 +1,7 @@
 package ariefbelajarteknologi.spring.core.service;
 
+import ariefbelajarteknologi.spring.core.data.User;
+import ariefbelajarteknologi.spring.core.event.LoginSuccessEvent;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.stereotype.Component;
@@ -7,10 +9,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserService implements ApplicationEventPublisherAware {
 
-    private ApplicationEventPublisher applicationEventPublisherAware;
+    private ApplicationEventPublisher applicationEventPublisher;
 
     @Override
     public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
-        this.applicationEventPublisherAware = applicationEventPublisher;
+        this.applicationEventPublisher = applicationEventPublisher;
     }
+
+    public boolean login(String username, String password) {
+        if (isLoginSuccess(username, password)) {
+            applicationEventPublisher.publishEvent(new LoginSuccessEvent(new User(username)));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isLoginSuccess(String username, String password) {
+        return "orang".equals(username) && "rahasia".equals(password);
+    }
+
 }
