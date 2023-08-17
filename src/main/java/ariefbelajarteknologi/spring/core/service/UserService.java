ba95f8a8470a061d8b097807ecdcca88c4c19e2b
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-18 02:52:00 +0700

    Event Listener, add service class

---
+++ src/main/java/ariefbelajarteknologi/spring/core/service/UserService.java
@@ -0,0 +1,16 @@
+package ariefbelajarteknologi.spring.core.service;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserService implements ApplicationEventPublisherAware {
+
+    private ApplicationEventPublisherAware applicationEventPublisherAware;
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        this.applicationEventPublisherAware = applicationEventPublisher;
+    }
+}
