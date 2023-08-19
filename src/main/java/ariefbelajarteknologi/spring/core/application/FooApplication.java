9313af351187fbaad05f46b72d05d2606c46dcc2
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-20 06:41:48 +0700

    Start Up Failure, modify application class

--- src/main/java/ariefbelajarteknologi/spring/core/application/FooApplication.java
+++ src/main/java/ariefbelajarteknologi/spring/core/application/FooApplication.java
@@ -1,5 +1,6 @@
 package ariefbelajarteknologi.spring.core.application;
 
+import ariefbelajarteknologi.spring.core.data.Bar;
 import ariefbelajarteknologi.spring.core.data.Foo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 public class FooApplication {
 
     @Bean
-    public Foo foo(){
+    public Foo foo(Bar bar){
         return new Foo();
     }
 
