9e231aeadf68e1d4f72c0f3fa32f4924e126b50a
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-21 08:30:42 +0700

    Application Runner, modify runnerapp class

--- src/main/java/ariefbelajarteknologi/spring/core/runnerapp/RunnerApplication.java
+++ src/main/java/ariefbelajarteknologi/spring/core/runnerapp/RunnerApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RunnerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RunnerApplication.class);
+        SpringApplication.run(RunnerApplication.class, args);
     }
 }
